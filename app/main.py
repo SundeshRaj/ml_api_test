a223f854b792333a810366815c82c3146e77b04a
Sundesh Raj <sundeshraj1993@gmail.com>
2024-03-11 16:47:17 -0500

    Updated main.py
    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 # Author: Sundesh
 # Date:   3/11/2024
 # Description:
+
+from fastapi import FastAPI
+from app.routers import inference, training, validation, performance
+
+app = FastAPI(title="ML Models API", version="1.0", description="An API for machine learning model operations including training, inference, validation, and performance analysis.")
+
+# Include the routers from the 'routers' module
+app.include_router(inference.router, tags=["Inference"], prefix="/inference")
+app.include_router(training.router, tags=["Training"], prefix="/training")
+app.include_router(validation.router, tags=["Validation"], prefix="/validation")
+app.include_router(performance.router, tags=["Performance"], prefix="/performance")
